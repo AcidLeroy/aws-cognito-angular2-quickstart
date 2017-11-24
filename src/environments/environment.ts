6db9f7d94345086f9c2e18836e954fdb3e53d713
Cody W. Eilar <Cody.Eilar@Gmail.com>
2017-11-24 11:57:17 -0700

    Change environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,15 @@
 export const environment = {
     production: false,
 
-    region: 'us-east-1',
+    region: 'us-west-2',
 
-    identityPoolId: 'us-east-1:fbe0340f-9ffc-4449-a935-bb6a6661fd53',
-    userPoolId: 'us-east-1_PGSbCVZ7S',
-    clientId: 'hh5ibv67so0qukt55c5ulaltk',
+    identityPoolId: 'us-west-2:5eb51562-adc8-4b12-ac54-9a01ec2b31513',
+    userPoolId: 'us-west-2_a2QEeA9PI',
+    clientId: '6virimip5201eiqg2k1dnjgahq',
 
     rekognitionBucket: 'rekognition-pics',
     albumName: "usercontent",
-    bucketRegion: 'us-east-1',
+    bucketRegion: 'us-west-2',
 
     ddbTableName: 'LoginTrail'
 };
-
